9d8dd9c1cb88815d8c4efca7699cfe6c2d773072
Guillermo Bernaldo de Quiros Maraver <debugbsd@gmail.com>
2020-03-31 11:16:25 +0200

    - Added a transform to the triangle.

--- Insanity/Main.cpp
+++ Insanity/Main.cpp
@@ -44,6 +44,7 @@
 
 const GLint HEIGHT = 768, WIDTH = 1024;
 GLuint VAO, VBO, shader, uniformModel;
+const float toRadians = 3.14159265f / 180.0f;
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -261,7 +262,9 @@ int main()
 
         // Transform
         glm::mat4 model(1.0f);
-        model = glm::translate(model, glm::vec3(triOffset, 0,0));
+        //model = glm::translate(model, glm::vec3(triOffset, 0,0));
+        //model = glm::rotate(model, 45.0f * toRadians, glm::vec3(0.0, 0.0f, 1.0f));
+        model = glm::scale(model, glm::vec3(0.4f, 0.4f, 0.0f));
 
         // Establecemos el valor al shader.
         glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
